16ec5a70b3448c6fc471e396617866fed1928d51
nariakiiwatani <nariakiiwatani@annolab.com>
2015-12-12 22:45:44 +0900

    removed rvalue callback

--- src/ofxLiquidEventUtils.h
+++ src/ofxLiquidEventUtils.h
@@ -30,10 +30,10 @@ namespace ofxLiquidEventUtils {
 	void addListener(const string &key, function<void(const ArgType&...)> func) {
 		getEvent<ArgType...>(key) += func;
 	}
-	template<class... ArgType>
-	void addListener(const string &key, function<void(ArgType&&...)> func) {
-		getEvent<ArgType...>(key) += [func](ArgType... arg) { func(forward<ArgType>(arg)...); };
-	}
+//	template<class... ArgType>
+//	void addListener(const string &key, function<void(ArgType&&...)> func) {
+//		getEvent<ArgType...>(key) += [func](ArgType... arg) { func(forward<ArgType>(arg)...); };
+//	}
 	
 	template<class Listener>
 	void addListener(const string &key, Listener *listener, void(Listener::*func)(void)) {
@@ -51,10 +51,10 @@ namespace ofxLiquidEventUtils {
 	void addListener(const string &key, Listener *listener, void(Listener::*func)(const ArgType&...)) {
 		getEvent<ArgType...>(key) += [listener,func](ArgType... arg) { (listener->*func)(arg...); };
 	}
-	template<class Listener, class... ArgType>
-	void addListener(const string &key, Listener *listener, void(Listener::*func)(ArgType&&...)) {
-		getEvent<ArgType...>(key) += [listener,func](ArgType... arg) { (listener->*func)(forward<ArgType>(arg)...); };
-	}
+//	template<class Listener, class... ArgType>
+//	void addListener(const string &key, Listener *listener, void(Listener::*func)(ArgType&&...)) {
+//		getEvent<ArgType...>(key) += [listener,func](ArgType... arg) { (listener->*func)(forward<ArgType>(arg)...); };
+//	}
 
 	// ==========
 	void notify(const string &key) {
@@ -68,10 +68,10 @@ namespace ofxLiquidEventUtils {
 	void notify(const string &key, const ArgType&... arg) {
 		getEvent<ArgType...>(key).notifyListeners(const_cast<ArgType&>(arg)...);
 	}
-	template<class... ArgType>
-	void notify(const string &key, ArgType&&... arg) {
-		getEvent<ArgType...>(key).notifyListeners(arg...);
-	}
+//	template<class... ArgType>
+//	void notify(const string &key, ArgType&&... arg) {
+//		getEvent<ArgType...>(key).notifyListeners(arg...);
+//	}
 	template<class... ArgType>
 	void notify(const string &key, ArgType*... arg) {
 		getEvent<ArgType*...>(key).notifyListeners(arg...);
